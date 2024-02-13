264a8c9fc6f575b340c6ba9cbc446e6e09898174
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:18:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-mouse-dev-133421",
+  url: "https://floral-mouse-dev-133421.botics.co",
+  api: "https://floral-mouse-dev-133421.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
